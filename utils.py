a121148223ba071860745cea12f6b03fbdf799a9
duwutape <raven.langheld@gmx.de>
2025-04-20 01:58:58 +0200

    feat: added util functions for grauer raum fix: fixed util functions for grauer raum (db)

--- utils.py
+++ utils.py
@@ -1,17 +1,34 @@
 import os
-from typing import Literal
+import sqlite3
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
 # grauer raum
-def get_modules():
-    #TODO get modules and return them as Literal
-    modules = Literal['']
-    return modules
+def connect_db():
+    db = sqlite3.connect(os.getenv('DB_PATH'))
+    cursor = db.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS altklausuren('
+                   'modul TEXT PRIMARY KEY,'
+                   'studiengang TEXT,'
+                   'beschreibung TEXT'
+                   'filepath TEXT'
+                   ');')
+    return cursor
 
-def get_path(modul: str):
-    GR_PATH = os.getenv('GR_PATH')
-    GR_FILE = os.getenv('GR_FILE')
 
-    return f'{GR_PATH}{modul}{GR_FILE}'
+def get_modules(cursor):
+    modules_tuples = cursor.execute('SELECT modul FROM altklausren ORDER BY modul;').fetchall()
+    modules = tuple_list_to_list(modules_tuples)
+    return [modul.lower() for modul in modules]
+
+def get_path(cursor, modul):
+    path_tuple = cursor.execute(f'SELECT filepath FROM altklausren where modul = {modul};').fetchone()
+    return tuple_to_str(path_tuple)
+
+def tuple_list_to_list(tuple_list):
+    return ['%s' % t for t in tuple_list]
+
+def tuple_to_str(tuple):
+    return ''.join(tuple)
